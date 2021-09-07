dc1cb95780a6059cc319a4543e3c0f167d0e8743
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-09-07 15:07:57 +0900

    Animal까지 완료

--- DJ_JSP/20210907/DJ_JSP_Servlet/src/main/java/com/answer/question/AnimalForm.java
+++ DJ_JSP/20210907/DJ_JSP_Servlet/src/main/java/com/answer/question/AnimalForm.java
@@ -1,6 +1,8 @@
 package com.answer.question;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,33 @@ public class AnimalForm extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		request.setCharacterEncoding("utf-8");
+		
+		String aName = request.getParameter("a_myname");
+		String aNumber = request.getParameter("a_mynumber");
+		int aAge = 0;
+		try {			
+			aAge = Integer.parseInt(request.getParameter("a_myage"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String aKinds = request.getParameter("a_mykinds");
+		
+		Animal myAnimal = new Animal(aKinds,aName,aAge,aNumber);
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head></head>");
+		writer.println("<body>");
+		writer.println("<h1>이름 : "+ myAnimal.getName() +"</h1>");
+		writer.println("<h1>나이 : "+ myAnimal.getAge() +"</h1>");
+		writer.println("<h1>번호 : "+ myAnimal.getNumber() +"</h1>");
+		writer.println("<h1>종류 : "+ myAnimal.getKinds() +"</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
 	}
 
 	/**
