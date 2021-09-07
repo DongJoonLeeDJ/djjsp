df659d0a99ab63fa049c88ef494ff956edf6370c
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-09-07 12:31:01 +0900

    서블릿+class+form

--- DJ_JSP/20210907/DJ_JSP_Servlet/src/main/java/com/start/servlet/FormEx.java
+++ DJ_JSP/20210907/DJ_JSP_Servlet/src/main/java/com/start/servlet/FormEx.java
@@ -31,10 +31,14 @@ public class FormEx extends HttpServlet {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		String name = request.getParameter("myname");
+		int age = Integer.parseInt(request.getParameter("myage"));
 		System.out.println(name);
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter writer = response.getWriter();
-		writer.println(name+"님 안녕");
+		//writer.println(name+"님 안녕");
+		
+		Student s = new Student(name, age);
+		writer.println(s.getName()+"님 안녕, 나이는 "+s.getAge()+"살이구나");
 	}
 
 	/**
@@ -42,7 +46,14 @@ public class FormEx extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		request.setCharacterEncoding("utf-8"); //Post방식은 이 걸 추가해줘야 함
+		String name = request.getParameter("myname_post");
+		System.out.println(name);
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		writer.println(name+"님 안녕하세요. Post입니다.");
+		
 	}
 
 }
