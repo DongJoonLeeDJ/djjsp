ed7430249f02b067dcc2de01fb07a0ade88a8386
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-09-13 14:07:22 +0900

    20210913
    20210913

---
+++ jpa02/src/main/java/com/mh/jpa02/Jpa02Application.java
@@ -0,0 +1,13 @@
+package com.mh.jpa02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa02Application.class, args);
+    }
+
+}
---
+++ jpa02/src/test/java/com/mh/jpa02/Jpa02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.jpa02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
