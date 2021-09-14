898f9f0a4df7757c05a923110cf34f12b76ef5fa
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-09-14 23:31:43 +0900

    c# 카카오 지도 api 사용

---
+++ cSharp/참고자료/지도_카카오api이용/지도/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace 지도
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Listbox = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(600, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "내용입력";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(713, 10);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.Text = "검색";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "지역이름";
+            // 
+            // Listbox
+            // 
+            this.Listbox.FormattingEnabled = true;
+            this.Listbox.ItemHeight = 12;
+            this.Listbox.Location = new System.Drawing.Point(12, 39);
+            this.Listbox.Name = "Listbox";
+            this.Listbox.Size = new System.Drawing.Size(120, 388);
+            this.Listbox.TabIndex = 3;
+            this.Listbox.SelectedIndexChanged += new System.EventHandler(this.Listbox_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(149, 39);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(639, 388);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.Url = new System.Uri("http://ehpub.co.kr/kakaomap.html", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.Listbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox Listbox;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ cSharp/참고자료/지도_카카오api이용/지도/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+
+
+namespace 지도
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class MyLocale
+
+        {
+
+
+            public string Name
+
+            {
+
+                get;
+
+                private set;
+
+            }
+
+            public double Lat
+
+            {
+
+                get;
+
+                private set;
+
+            }
+
+
+
+            public double Lng
+
+            {
+
+                get;
+
+                private set;
+
+            }
+
+
+
+            public MyLocale(string name, double lat, double lng)
+
+            {
+
+                Name = name;
+
+                Lat = lat;
+
+                Lng = lng;
+
+            }
+
+            public override string ToString()
+
+            {
+
+                return Name;
+
+            }
+
+        }
+        public class KaKaoAPI
+
+        {
+
+
+
+            public static List<MyLocale> Search(string qstr)
+
+            {
+
+                string site = "https://dapi.kakao.com/v2/local/search/keyword.json";
+
+                string query = string.Format("{0}?query={1}", site, qstr);
+
+
+
+      
+
+                string rkey = "6c8825b9af60f0a4f8d0d34f86e89e2b";
+
+                string header = "KakaoAK " + rkey;
+                   WebRequest request = WebRequest.Create(query);
+
+                request.Headers.Add("Authorization", header);
+             
+
+
+
+                WebResponse response = request.GetResponse();
+
+                Stream stream = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+                String json = reader.ReadToEnd();
+
+
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+
+                dynamic dob = js.Deserialize<dynamic>(json);
+
+                dynamic docs = dob["documents"];
+
+                object[] buf = docs;
+
+                int length = buf.Length;
+
+
+
+                List<MyLocale> mls = new List<MyLocale>();
+
+                for (int i = 0; i < length; i++)
+
+                {
+
+                    string lname = docs[i]["place_name"];
+
+                    double x = double.Parse(docs[i]["x"]);
+
+                    double y = double.Parse(docs[i]["y"]);
+
+                    mls.Add(new MyLocale(lname, y, x));
+
+                }
+
+                return mls;
+
+            }
+
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string qstr = textBox1.Text;
+
+            List<MyLocale> mls = KaKaoAPI.Search(qstr);
+
+            Listbox.Items.Clear();
+            foreach(MyLocale locale in mls)
+            {
+                Listbox.Items.Add(locale);
+            }
+        }
+
+        private void Listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(Listbox.SelectedIndex == -1)
+            {
+                return;
+            }
+            MyLocale ml = Listbox.SelectedItem as MyLocale;
+            object[] ps = new object[] { ml.Lat, ml.Lng };
+            HtmlDocument hdoc = webBrowser1.Document;
+            hdoc.InvokeScript("setCenter", ps);
+
+
+        }
+    }
+
+
+   
+
+
+}
