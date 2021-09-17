df5c86c2fe586c36a97b843eabc8d23eb938b2aa
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-09-17 15:12:44 +0900

    20210917
    20210917

--- jpa02/src/main/java/com/mh/jpa02/model/Board.java
+++ jpa02/src/main/java/com/mh/jpa02/model/Board.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -16,6 +17,8 @@ public class Board {
     private long id;
 
     private String name;
+
+    @Size(min=2, max=30, message = "제목은 2이상 30이하 길이여야합니다.")
     private String title;
     private String content;
     private String date;
